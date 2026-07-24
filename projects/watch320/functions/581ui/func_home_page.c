9a36b553e1976f64f1d61cb08f17d87cc3827981
tkx <tkx>
2026-07-24 09:20:11 +0800

    首页ui

--- projects/watch320/functions/581ui/func_home_page.c
+++ projects/watch320/functions/581ui/func_home_page.c
@@ -7,64 +7,38 @@
 #define TRACE(...)
 #endif
 
-//备份模式选中状态
-static void select_backup_mode(compo_form_t *frm)
+// 首页私有状态
+typedef struct {
+    u8 selection;  // 0 = 备份模式, 1 = 设置
+    compo_picturebox_t *pic_backup;
+    compo_picturebox_t *pic_set;
+    compo_textbox_t *txt_backup;
+    compo_textbox_t *txt_set;
+} f_home_t;
+
+static void home_update_display(void)
 {
-    compo_textbox_t *txt1;
-    compo_textbox_t *txt2;
-    /* 上方: 备份模式图标 */
-    compo_picturebox_t *pic_backup = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_BACKUP_MODE_1_BIN);
-    compo_picturebox_set_pos(pic_backup, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y - 24);
+    f_home_t *h = (f_home_t *)func_cb.f_cb;
+    bool is_backup = (h->selection == 0);
 
-    txt1 = compo_textbox_create(frm, 7);
-    compo_textbox_set_location(txt1, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y - 24, 64, 23);
-    compo_textbox_set_align_center(txt1, true);
-    compo_textbox_set_forecolor(txt1, COLOR_BLACK);
-    compo_textbox_set(txt1, i18n[STR_BACKUP]);
+    // 备份模式：选中态 vs 普通态
+    compo_picturebox_set(h->pic_backup, is_backup
+        ? UI_BUF_IMAGE_BIN_BACKUP_MODE_1_BIN
+        : UI_BUF_IMAGE_BIN_BACKUP_MODE_BIN);
+    compo_textbox_set_forecolor(h->txt_backup, is_backup ? COLOR_BLACK : COLOR_WHITE);
 
-    /* 下方: 设置图标 */
-    compo_picturebox_t *pic_set = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_SET_BIN);
-    compo_picturebox_set_pos(pic_set, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y + 24);
-
-    txt2 = compo_textbox_create(frm, 7);
-    compo_textbox_set_location(txt2, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y + 24, 64, 23);
-    compo_textbox_set_align_center(txt2, true);
-    compo_textbox_set_forecolor(txt2, COLOR_WHITE);
-    compo_textbox_set(txt2, i18n[STR_SET]);
-}
-
-// 设置模式选中状态
-static void select_set(compo_form_t *frm)
-{
-    compo_textbox_t *txt1;
-    compo_textbox_t *txt2;
-    /* 上方: 备份模式图标 */
-    compo_picturebox_t *pic_backup = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_BACKUP_MODE_BIN);
-    compo_picturebox_set_pos(pic_backup, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y - 24);
-
-    txt1 = compo_textbox_create(frm, 7);
-    compo_textbox_set_location(txt1, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y - 24, 64, 23);
-    compo_textbox_set_align_center(txt1, true);
-    compo_textbox_set_forecolor(txt1, COLOR_WHITE);
-    compo_textbox_set(txt1, i18n[STR_BACKUP]);
-
-    /* 下方: 设置图标 */
-    compo_picturebox_t *pic_set = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_SET_1_BIN);
-    compo_picturebox_set_pos(pic_set, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y + 24);
-
-    txt2 = compo_textbox_create(frm, 7);
-    compo_textbox_set_location(txt2, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y + 24, 64, 23);
-    compo_textbox_set_align_center(txt2, true);
-    compo_textbox_set_forecolor(txt2, COLOR_BLACK);
-    compo_textbox_set(txt2, i18n[STR_SET]);
+    // 设置：选中态 vs 普通态
+    compo_picturebox_set(h->pic_set, is_backup
+        ? UI_BUF_IMAGE_BIN_SET_BIN
+        : UI_BUF_IMAGE_BIN_SET_1_BIN);
+    compo_textbox_set_forecolor(h->txt_set, is_backup ? COLOR_WHITE : COLOR_BLACK);
 }
 
 compo_form_t *func_home_page_form_create(void)
 {
+    f_home_t *h = (f_home_t *)func_cb.f_cb;
     compo_form_t *frm = compo_form_create(true);
     compo_textbox_t *txt;
-    compo_textbox_t *txt1;
-    compo_textbox_t *txt2;
 
     /* 黑色背景，避免 GPU 误读 UI 头当图解码 */
     widget_set_visible(frm->icon, false);
@@ -74,11 +48,31 @@ compo_form_t *func_home_page_form_create(void)
                              GUI_SCREEN_WIDTH, GUI_SCREEN_HEIGHT);
 
     txt = compo_textbox_create(frm, 4);
-    compo_textbox_set_location(txt, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y-80, 200, 40);
+    compo_textbox_set_location(txt, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y - 80, 200, 40);
     compo_textbox_set_align_center(txt, true);
     compo_textbox_set(txt, i18n[STR_HOME]);
 
-    select_backup_mode(frm);
+    /* 上方：备份模式图标 */
+    h->pic_backup = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_BACKUP_MODE_1_BIN);
+    compo_picturebox_set_pos(h->pic_backup, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y - 24);
+
+    h->txt_backup = compo_textbox_create(frm, 7);
+    compo_textbox_set_location(h->txt_backup, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y - 24, 64, 23);
+    compo_textbox_set_align_center(h->txt_backup, true);
+    compo_textbox_set(h->txt_backup, i18n[STR_BACKUP]);
+
+    /* 下方：设置图标 */
+    h->pic_set = compo_picturebox_create(frm, UI_BUF_IMAGE_BIN_SET_BIN);
+    compo_picturebox_set_pos(h->pic_set, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y + 24);
+
+    h->txt_set = compo_textbox_create(frm, 7);
+    compo_textbox_set_location(h->txt_set, GUI_SCREEN_CENTER_X - 30, GUI_SCREEN_CENTER_Y + 24, 64, 23);
+    compo_textbox_set_align_center(h->txt_set, true);
+    compo_textbox_set(h->txt_set, i18n[STR_SET]);
+
+    /* 初始选中备份模式 */
+    compo_textbox_set_forecolor(h->txt_backup, COLOR_BLACK);
+    compo_textbox_set_forecolor(h->txt_set, COLOR_WHITE);
 
     return frm;
 }
@@ -90,8 +84,26 @@ static void func_home_page_process(void)
 
 static void func_home_page_message(size_msg_t msg)
 {
+    f_home_t *h = (f_home_t *)func_cb.f_cb;
+
     switch (msg)
     {
+    case MSG_QDEC_FORWARD:
+    case MSG_CTP_SHORT_DOWN:
+        if (h->selection != 1) {
+            h->selection = 1;
+            home_update_display();
+        }
+        break;
+
+    case MSG_QDEC_BACKWARD:
+    case MSG_CTP_SHORT_UP:
+        if (h->selection != 0) {
+            h->selection = 0;
+            home_update_display();
+        }
+        break;
+
     default:
         func_message(msg);
         break;
@@ -100,6 +112,7 @@ static void func_home_page_message(size_msg_t msg)
 
 void func_home_page_enter(void)
 {
+    func_cb.f_cb = func_zalloc(sizeof(f_home_t));
     func_cb.frm_main = func_home_page_form_create();
 }
 
