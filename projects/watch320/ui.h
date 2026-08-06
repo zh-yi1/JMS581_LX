781d39568e065fc196230bd0abcd18d9471c62fa
正心 <13217265055@163.com>
2026-08-06 15:43:55 +0800

    首页显示

--- projects/watch320/ui.h
+++ projects/watch320/ui.h
@@ -26,19 +26,49 @@
 #define UI_BUF_FONT_BIN_FONT_F18_BIN               UI_ADDR_BASE(0x1d836)
 #define UI_LEN_FONT_BIN_FONT_F18_BIN               0x1cf5
 
-#define UI_BUF_ICON_ACTIVITY_BIN                   UI_ADDR_BASE(0x1f52b)
+#define UI_BUF_FONT_BIN_FONT_SIZE_11_BIN           UI_ADDR_BASE(0x1f52b)
+#define UI_LEN_FONT_BIN_FONT_SIZE_11_BIN           0x707f
+
+#define UI_BUF_FONT_BIN_FONT_SIZE_15_BIN           UI_ADDR_BASE(0x265aa)
+#define UI_LEN_FONT_BIN_FONT_SIZE_15_BIN           0x858f
+
+#define UI_BUF_FONT_BIN_FONT_SIZE_17_BIN           UI_ADDR_BASE(0x2eb39)
+#define UI_LEN_FONT_BIN_FONT_SIZE_17_BIN           0x91b5
+
+#define UI_BUF_FONT_BIN_FONT_SIZE_22_BIN           UI_ADDR_BASE(0x37cee)
+#define UI_LEN_FONT_BIN_FONT_SIZE_22_BIN           0xbe72
+
+#define UI_BUF_ICON_ACTIVITY_BIN                   UI_ADDR_BASE(0x43b60)
 #define UI_LEN_ICON_ACTIVITY_BIN                   0x13c3
 
-#define UI_BUF_IMAGE_BIN_BACKUP_MODE_BIN           UI_ADDR_BASE(0x208ee)
+#define UI_BUF_IMAGE_BIN_BACKUP_MODE_BIN           UI_ADDR_BASE(0x44f23)
 #define UI_LEN_IMAGE_BIN_BACKUP_MODE_BIN           0x313
 
-#define UI_BUF_IMAGE_BIN_BACKUP_MODE_1_BIN         UI_ADDR_BASE(0x20c01)
+#define UI_BUF_IMAGE_BIN_BACKUP_MODE_1_BIN         UI_ADDR_BASE(0x45236)
 #define UI_LEN_IMAGE_BIN_BACKUP_MODE_1_BIN         0x237
 
-#define UI_BUF_IMAGE_BIN_SET_BIN                   UI_ADDR_BASE(0x20e38)
+#define UI_BUF_IMAGE_BIN_BOTTON_CLICK_BIN          UI_ADDR_BASE(0x4546d)
+#define UI_LEN_IMAGE_BIN_BOTTON_CLICK_BIN          0x319
+
+#define UI_BUF_IMAGE_BIN_BOTTON_UNCLICK_BIN        UI_ADDR_BASE(0x45786)
+#define UI_LEN_IMAGE_BIN_BOTTON_UNCLICK_BIN        0x319
+
+#define UI_BUF_IMAGE_BIN_CLICK_BIN                 UI_ADDR_BASE(0x45a9f)
+#define UI_LEN_IMAGE_BIN_CLICK_BIN                 0x14e
+
+#define UI_BUF_IMAGE_BIN_CLICK_BJ_BIN              UI_ADDR_BASE(0x45bed)
+#define UI_LEN_IMAGE_BIN_CLICK_BJ_BIN              0x369
+
+#define UI_BUF_IMAGE_BIN_DIVIDER_UNCLICK_BIN       UI_ADDR_BASE(0x45f56)
+#define UI_LEN_IMAGE_BIN_DIVIDER_UNCLICK_BIN       0x369
+
+#define UI_BUF_IMAGE_BIN_SET_BIN                   UI_ADDR_BASE(0x462bf)
 #define UI_LEN_IMAGE_BIN_SET_BIN                   0x2fc
 
-#define UI_BUF_IMAGE_BIN_SET_1_BIN                 UI_ADDR_BASE(0x21134)
+#define UI_BUF_IMAGE_BIN_SET_1_BIN                 UI_ADDR_BASE(0x465bb)
 #define UI_LEN_IMAGE_BIN_SET_1_BIN                 0x220
+
+#define UI_BUF_IMAGE_BIN_UNCLICK_BIN               UI_ADDR_BASE(0x467db)
+#define UI_LEN_IMAGE_BIN_UNCLICK_BIN               0x15f
 
 #endif
